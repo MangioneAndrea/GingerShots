cd61287544348b134857c56c5081f7efc17b9005
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2021-08-20 14:08:55 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { MaterialAppMin } from "svelte-materialify";
+	import NavBar from "./components/NavBar.svelte";
+	import Home from "./routes/Home.svelte";
+
+	let page = Home;
+	let theme = localStorage.getItem("theme") || "light";
+	$: {
+		if (theme) {
+			localStorage.setItem("theme", theme);
+		}
+	}
+</script>
+
+<MaterialAppMin {theme}>
+	<NavBar bind:page bind:theme />
+	<svelte:component this={page} />
+</MaterialAppMin>
+
+<style>
+	:global(body) {
+		padding: 0;
+	}
+</style>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,64 @@
+<script>
+  import { Button, Icon, AppBar, Divider, Tabs, Tab } from "svelte-materialify";
+  import {
+    mdiThemeLightDark,
+    mdiViewDashboard,
+    mdiStar,
+    mdiClipboardList,
+  } from "@mdi/js";
+  import Home from "../routes/Home.svelte";
+  import List from "../routes/List.svelte";
+  import Reviews from "../routes/Reviews.svelte";
+
+  export let page;
+  export let theme;
+
+  const tabs = [
+    { icon: mdiViewDashboard, link: Home },
+    { icon: mdiClipboardList, link: List },
+    { icon: mdiStar, link: Reviews },
+  ];
+
+  const switchTheme = () => {
+    theme = theme === "light" ? "dark" : "light";
+  };
+
+  const changePage = (evt) => {
+    page = tabs[evt.detail]?.link || "home";
+  };
+</script>
+
+<AppBar dense>
+  <span slot="title">Gin-ger shots</span>
+  <div style="flex-grow:1" />
+  <div class="tabs">
+    <Tabs centered fixedTabs on:change={changePage}>
+      <div slot="tabs">
+        {#each tabs as tab}
+          <Tab><Icon path={tab.icon} /></Tab>
+        {/each}
+      </div>
+    </Tabs>
+  </div>
+  <Divider vertical />
+  <div class="staticButtons">
+    <Button class="topBarRightButtons" depressed on:click={switchTheme}
+      >Login</Button
+    >
+    <Button class="topBarRightButtons" icon on:click={switchTheme}>
+      <Icon path={mdiThemeLightDark} />
+    </Button>
+  </div>
+</AppBar>
+
+<style type="text/scss">
+  .tabs {
+    margin: auto;
+  }
+  .staticButtons {
+    padding: 10px;
+    :global(.topBarRightButtons) {
+      margin: auto;
+    }
+  }
+</style>
